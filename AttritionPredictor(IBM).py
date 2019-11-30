c68e769600faa16190dfeb26085bf32b541f54dd
rkporwal <rkporwal.3025@gmail.com>
2019-11-30 20:50:53 +0530

    Initial commit- have loaded dependencies and data

---
+++ AttritionPredictor(IBM).py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv("Employee-Attrition.csv")
+
+dataframe.info()
+dataframe.describe()
+
+# Exploratory analysis 
