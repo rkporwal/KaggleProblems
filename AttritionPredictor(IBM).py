1e4fc6f4a769535fdb41c2333dc04277d9d21ab7
rkporwal <rkporwal.3025@gmail.com>
2019-12-28 11:53:50 +0530

    Initial commit- have loaded dependencies and data

--- AttritionPredictor(IBM).py
+++ AttritionPredictor(IBM).py
@@ -7,4 +7,29 @@ dataframe = pd.read_csv("Employee-Attrition.csv")
 dataframe.info()
 dataframe.describe()
 
-# Exploratory analysis 
+# Exploratory data analysis 
+
+dataframe['Attrition'].unique()
+dataframe['Attrition'].value_counts()
+
+categorical_features=[]
+for col in dataframe.columns:
+    if(dataframe[col].dtype=='object'):
+        categorical_features.append(col)
+        
+for col in categorical_features:
+            print(col,":",len(dataframe[col].unique()))
+            
+
+plt.scatter(dataframe['Age'],dataframe['Attrition'])          
+          
+plt.scatter(dataframe['DailyRate'],dataframe['Attrition']) 
+plt.scatter(dataframe['DistanceFromHome'],dataframe['Attrition']) 
+plt.scatter(dataframe['Education'],dataframe['Attrition']) 
+plt.scatter(dataframe['EnvironmentSatisfaction'],dataframe['Attrition']) 
+plt.scatter(dataframe['JobSatisfaction'],dataframe['Attrition']) 
+plt.scatter(dataframe['YearsAtCompany'],dataframe['Attrition']) 
+
+# features importance, 
+correlation_matrix=dataframe.corr()
+
--- Titanic_ExploratoryAnalysis.py
+++ Titanic_ExploratoryAnalysis.py
@@ -2,7 +2,80 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import train_test_split
 # importing dataset
 dataset=pd.read_csv("titanic_train.csv")
 
+dataset.head()
+dataset.info()
+dataset.isnull()
+dataset.isnull().sum()
+
+sns.headmap()
+
+# Let’s do some Exploratory Data Analysis
+sns.heatmap(dataset.isnull(),yticklabels=False,cbar='viridis')
+
+# let’s see what is the ratio of survived v/s not survived.
+#people who survived v/s who didn't
+
+sns.set_style('whitegrid')
+sns.countplot(x='Survived', data=dataset, palette='RdBu_r')
+
+sns.countplot(x='Survived', hue='Sex', data= dataset,palette='RdBu_r')
+
+sns.countplot(x='Survived', hue='Pclass', data= dataset, palette='rainbow')
+
+sns.distplot(dataset['Age'].dropna(),color='darkred',bins=30)
+
+
+dataset['Fare'].hist(color='green',bins=40,figsize=(8,4))
+
+# Data Cleaning
+plt.figure(figsize=(12, 7))
+sns.boxplot(x='Pclass',y='Age',data=dataset,palette='winter')
+
+
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+    if pd.isnull(Age):
+        if Pclass == 1:
+            return 37
+        elif Pclass == 2:
+            return 29
+        else:
+            return 24
+    else:
+            return Age
+
+
+dataset['Age'] = dataset[['Age', 'Pclass']].apply(impute_age, axis = 1)   
+
+def impute_cabin(col):
+  Cabin = col[0]
+  if type(Cabin) == str:
+    return 1
+  else:
+      return 0
+   
+dataset['Cabin'] = dataset[['Cabin']].apply(impute_cabin, axis = 1)
+
+
+sex = pd.get_dummies(dataset['Sex'],drop_first=True)
+embark = pd.get_dummies(dataset['Embarked'],drop_first=True)
+dataset['Embarked'].unique()
+dataset.drop(['Sex','Embarked','Name','Ticket'],axis=1,inplace=True)
+dataset = pd.concat([dataset,sex,embark],axis=1)
+dataset.head()
+
+
+X_train, X_test, y_train, y_test = train_test_split(dataset.drop('Survived',axis=1),dataset['Survived'], test_size=0.25,random_state=101)
+
+from sklearn.linear_model import LogisticRegression
+regressor = LogisticRegression()
+regressor.fit(X_train, y_train)
+pred = regressor.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, pred))
