8abbde75697840c41d8196ff6511d03e2b529029
rkporwal <rkporwal.3025@gmail.com>
2020-01-01 17:51:41 +0530

    Initial commit- have loaded dependencies and data

---
+++ HousePrice_BengaluruData.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+# importing dataset
+dataset=pd.read_csv("Bengaluru_House_Data.csv")
